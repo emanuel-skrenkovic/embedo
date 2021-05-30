50a609ea4f5f095c822a4ab7fc983bcb111da193
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2021-05-30 19:36:04 +0200

    small cleanup; added short clarification

--- src/lib.rs
+++ src/lib.rs
@@ -13,36 +13,60 @@ extern {
 const LOWER_DIFF: u32 = 32;
 
 #[wasm_bindgen]
-#[no_mangle]
 pub fn ceasar_cipher(text: &str, n: u32) -> String {
     let mut result: String = String::new();
 
     for c in text.chars() {
-        let diff = if c.is_uppercase() { 0 } else { LOWER_DIFF };
+        let is_upper = c.is_uppercase();
 
-        let ascii_char = c as u32;
-        let enciphered = (ascii_char + n - diff - 65) % 26;
-
-        let encrypted_ascii_char = enciphered as u8 + 65 + diff as u8;
-        result += std::str::from_utf8(&[encrypted_ascii_char]).unwrap();
+        let encrypted = (normalize_char(c as u32, is_upper) + n) % 26;
+        result += std::str::from_utf8(&[asciify_char(encrypted, is_upper) as u8]).unwrap();
     }
 
     result
 }
 
 #[wasm_bindgen]
-#[no_mangle]
 pub fn ceasar_decipher(text: &str, n: u32) -> String {
     let mut result: String = String::new();
 
     for c in text.chars() {
-        let diff = if c.is_uppercase() { 0 } else { LOWER_DIFF };
-
-        let deciphered = (c as u32 - n - diff - 65) % 26;
-
-        let deciphered_ascii_char = deciphered as u8 + 65 + diff as u8;
-        result += std::str::from_utf8(&[deciphered_ascii_char]).unwrap();
+        let is_upper = c.is_uppercase();
+        
+        let deciphered = (normalize_char(c as u32, is_upper) - n) % 26;
+        result += std::str::from_utf8(&[asciify_char(deciphered, is_upper) as u8]).unwrap();
     }
 
     result
+}
+
+fn normalize_char(c: u32, is_upper: bool) -> u32 {
+    /*
+     Since ASCII is being used, need to 'normalize'
+     the char. In ASCII, lowercase characters are higher in
+     number by 32 from their uppercase equivalents.
+    */
+    let diff = if is_upper { 0 } else { LOWER_DIFF };
+
+    /*
+     In ASCII, letters start at 65 so we subtract it
+     (along with the diff above) to get to the 0-26 range.
+    */
+    c - diff - 65
+}
+
+fn asciify_char(c: u32, is_upper: bool) -> u32 {
+    /*
+    Since ASCII is being used, need to 'normalize'
+    the char. In ASCII, lowercase characters are higher in
+    number by 32 from their uppercase equivalents.
+     */
+    let diff = if is_upper { 0 } else { LOWER_DIFF };
+
+    /*
+     This time we simply reverse the 'normalization' operation.
+     Add up 65 and the diff to the char to get back to the
+     ASCII range with correct capitalization.
+    */
+    c + 65 + diff
 }
